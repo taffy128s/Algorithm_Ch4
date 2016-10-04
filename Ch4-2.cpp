4499cbba66b835766a30b94f2317e3abbc74d246
Taffy Cheng <taffy128s@gmail.com>
2016-10-04 15:32:00 +0800

    Add 4-2.

---
+++ Ch4-2.cpp
@@ -0,0 +1,18 @@
+#include <cstdio>
+
+struct Item {
+    Item(int a, int b, int c) {
+        left = a;
+        right = b;
+        max = c;
+    }
+    int left, right, max;
+};
+
+int arr[1000] = {0, 13, -3, -25, 20, -3, -16, -23, 18, 20, -7, 12, -5, -22, 15, -4, 7};
+
+
+
+int main() {
+    return 0;
+}
