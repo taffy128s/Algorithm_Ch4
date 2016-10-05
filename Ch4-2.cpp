8ee96bf57af82e9af68794146db67164afb1dac0
Taffy Cheng <taffy128s@gmail.com>
2016-10-05 11:13:35 +0800

    4-2.

--- Ch4-2.cpp
+++ Ch4-2.cpp
@@ -10,9 +10,24 @@ struct Item {
 };
 
 int arr[1000] = {0, 13, -3, -25, 20, -3, -16, -23, 18, 20, -7, 12, -5, -22, 15, -4, 7};
-
-
+int size = 16;
+Item ans = Item(0, 0, 0);
 
 int main() {
+    ans.max = 1 << 31;
+    int sum = 0, templeftbound = 0;
+    for (int i = 1; i <= size; i++) {
+        if (sum > 0) sum += arr[i];
+        else {
+            sum = arr[i];
+            templeftbound = i;
+        }
+        if (sum > ans.max) {
+            ans.max = sum;
+            ans.left = templeftbound;
+            ans.right = i;
+        }
+    }
+    printf("max: %d left: %d right: %d\n", ans.max, ans.left, ans.right);
     return 0;
 }
